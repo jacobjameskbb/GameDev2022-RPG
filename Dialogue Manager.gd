2c244806f74b79eea11e62b31c9e83cb2e28a724
jacobjameskbb <jacob.james.kbb@gmail.com>
2022-09-21 14:22:07 -0700

    Fixed and Outlined Dialogue Manager

---
+++ Dialogue Manager.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+### Dialogue Manager ###
+
+# Should display dialogue messages in a textbox as needed
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	pass
+
+# Main function for dialogue, message should be a string
+func send_message(message):
+	print(message)
+	# Display textbox
+	
+	# Type message (using tween)
+	
+	# wait for input from user to continue 
