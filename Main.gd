11bf91033123aafc8c05d5fb8a51510d183050a9
jacobjameskbb <jacob.james.kbb@gmail.com>
2022-09-21 12:08:57 -0700

    Set Up Project

---
+++ Main.gd
@@ -0,0 +1,12 @@
+extends Node
+
+### Main Node ###
+
+# This node's code should be used to coordinate the world and menu manager
+# Things such as pausing, starting the game, and ending the game
+
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	pass
---
+++ World Manager.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+### World Manager ###
+
+# World manager should control everything that happens in-game
+# World manager should contain things such as the player, the environment, the enemies, etc.
+
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	pass
