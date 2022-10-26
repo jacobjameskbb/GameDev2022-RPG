b66fe53d185284c9c0610d955df0aec17e60a0b8
Thealfredjam <alfredjamsam@gmail.com>
2022-10-26 14:17:03 -0700

    added enemy but mow level 1 is ver buggy
    please help. I added an enemy and that bugged out level 1. I also added a tile set

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -2,11 +2,35 @@ extends KinematicBody2D
 
 var is_moving_left = true
 
-var gravity =  10 
+var gravity =  10 # check https://www.youtube.com/watch?v=jQKxOEbbirA for more detail
 var velocity = Vector2(0, 0)
-var speed = 32 # pixels per second
-var motion = Vector2()
-#var player = get_tree().get_root().get_node("res://Player.tscn")
 
-#func _on_Area2D_body_entered(body:player):
-	#pass # Replace with function body.
+var speed = 32 # pixels per second
+
+func _process(_delta):
+	move_character()
+	detect_turn_around()
+		
+func move_character():
+	velocity.x = -speed if is_moving_left else speed
+	velocity.y += gravity
+	
+	velocity = move_and_slide(velocity, Vector2.UP)
+
+func detect_turn_around():
+	if not $RayCast2D.is_colliding() and is_on_floor():
+		is_moving_left = !is_moving_left
+		scale.x = -scale.x # makes body flip
+
+func hit():
+	$AttackDetector.monitoring = true
+
+func end_of_hit():
+	$AttackDetector.monitoring = false
+	
+func _on_AttackDetector_body_entered(body):
+	get_tree().reload_current_scene()
+
+
+func _on_PlayerDetector_body_entered(body: Node) -> void:
+	pass 
