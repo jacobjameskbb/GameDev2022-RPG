306cd635d7536e48285d7ab799d4ba12cdaa434d
jacobjameskbb <jacob.james.kbb@gmail.com>
2022-10-26 13:24:58 -0700

    Fixed Menus
    - Fixed Menu Pausing
    - Fixed Menu location
    - Fixed Esc triggering the menu
    - Increased Player Speed

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,10 +1,10 @@
 extends Control
+	
+	
+signal play_pressed
 		
 func _on_Quit_Game_pressed():
 	get_tree().quit()
 
 func _on_play_pressed():
-	# The change scene functions could end badly, we don't want to swap this stuff around too much
-	#get_tree().change_scene("res://scenes/main.tscn")
-	#get_tree().change_scene("res://levels/level"+str(int(get_tree().current_scene.filename[-6])+1)+".tscn")
-	pass
+	emit_signal("play_pressed")
