6e9fdb615ca59f6fc0010c718b7b44ab1826353d
Thealfredjam <alfredjamsam@gmail.com>
2022-12-03 16:36:38 -0800

    changes

--- plant.gd
+++ plant.gd
@@ -1,12 +1,23 @@
-extends RigidBody2D
+extends StaticBody2D
 
-var bullet_speed = 2000
-var bullet = preload("res://plant_bullet.tscn")
+onready var bullet = preload("res://plant_bullet.tscn")
+var player_position = Vector2.ZERO
+#func fire():
+	#bullet_instance.position = player_position
+	#bullet_instance.rotation_degrees = rotation_degrees
+	#bullet_instance.apply_impulse(Vector2(),Vector2(bullet_speed,0).rotated(rotation))
+	#get_tree().get_root().call_deferred("add_child",bullet_instance)
+	
+
+func _on_Timer_timeout() -> void:
+	var current_bullet = bullet.instance()
+	#current_bullet.facing = 'left'
+	current_bullet.direction = self.position - player_position
+	add_child(current_bullet)
 
 
-func fire():
-	var bullet_instance = bullet.instance()
-	#bullent_instance.position = 
-	#bullent_instance.rotation_degrees = rotation_degrees
-	#bullent_instance.apply_impulse(Vector2(),Vector2(bullet_speed,0).rotated(rotation))
-	#get_tree().get_root().call_deferred("add_child",bullent_instance)
+
+func _on_Area2D_body_entered(body: Node) -> void:
+	if body.is_in_group("player"):
+		var player_position = body.postion
+		$Timer.start()
--- plant_bullet.gd
+++ plant_bullet.gd
@@ -1,2 +1,7 @@
 extends RigidBody2D
 
+var bullet_speed = 2000
+var direction = Vector2.ZERO
+
+func _process(delta: float) -> void:
+	self.position += bullet_speed * direction
